cf71ec9d45d98256e60f9dd479e57ca8a361e00b
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-05-03 14:52:49 +0800

    Prevent symlink on local dir as that causes issues especially when files are downloaded in docker containers

--- HFUtilDownload.py
+++ HFUtilDownload.py
@@ -11,13 +11,15 @@ class HFUtilDownload(HFUtilBase):
     def download_folder_from_repo(self, repo_id: str, folder_name: str, local_dir: str):
         files_list = [file for file in list(self.get_file_list(repo_id)) if f"{folder_name}/" in file.path]
         for file in tqdm(files_list):
-            self.hf_api.hf_hub_download(repo_id=repo_id, filename=file.path, token=self._read_token, local_dir=local_dir)
+            self.hf_api.hf_hub_download(repo_id=repo_id, filename=file.path, token=self._read_token,
+                                        local_dir=local_dir, local_dir_use_symlinks=False)
 
     def _download_file_from_repo(self, repo_id: str, local_dir: str, filename: str):
         files_list = os.listdir(local_dir)
         if filename not in files_list:
             print("downloading", filename)
-        self.hf_api.hf_hub_download(repo_id=repo_id, filename=filename, token=self._read_token, local_dir=local_dir)
+        self.hf_api.hf_hub_download(repo_id=repo_id, filename=filename, token=self._read_token, local_dir=local_dir,
+                                    local_dir_use_symlinks=False)
 
     def download_tokenizer_from_repo(self, repo_id: str, local_dir: str):
         print(local_dir)
@@ -39,14 +41,15 @@ class HFUtilDownload(HFUtilBase):
 
         if not tokenizer_exist:
             print("downloading tokenizer model")
-            snapshot_download(repo_id=repo_id, allow_patterns="*.model", local_dir=local_dir)
+            snapshot_download(repo_id=repo_id, allow_patterns="*.model", local_dir=local_dir,
+                              local_dir_use_symlinks=False)
 
     def download_config_from_repo(self, repo_id: str, local_dir: str):
         file_list = os.listdir(local_dir)
         if "config.json" not in file_list:
             print("downloading config.json")
             self.hf_api.hf_hub_download(repo_id=repo_id, filename="config.json", token=self._read_token,
-                                        local_dir=local_dir)
+                                        local_dir=local_dir, local_dir_use_symlinks=False)
 
 
 if __name__ == "__main__":
